73fab94f077e8aa23ed098ac0532437a08bc3abb
Maitane <gallastegui.maitane@uni.eus>
2022-12-22 09:28:24 +0100

    Modal with data

--- DidaktikApp/src/app/modal/modal.page.ts
+++ DidaktikApp/src/app/modal/modal.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-modal',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalPage implements OnInit {
 
+  title:string;
   constructor() { }
 
   ngOnInit() {
+    console.log(this.title);
   }
 
 }
