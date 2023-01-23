96d9cc6777b7a903e8134dee938f6c8dc4d50568
JonLopez02 <lopez.jon@uni.eus>
2023-01-23 09:07:57 +0100

    Informazioa

--- DidaktikApp/src/app/app-routing.module.ts
+++ DidaktikApp/src/app/app-routing.module.ts
@@ -22,7 +22,11 @@ const routes: Routes = [
   {
     path: 'modal',
     loadChildren: () => import('./modal/modal.module').then( m => m.ModalPageModule)
+  },  {
+    path: 'informazioa',
+    loadChildren: () => import('./informazioa/informazioa.module').then( m => m.InformazioaPageModule)
   },
+
 ];
 
 @NgModule({
